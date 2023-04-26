2f3112f25cf34cc8ce5ae5e8557ecf8361aea41f
Ng Zhe Kai <ngzhekai@gmail.com>
2023-04-26 23:09:22 +0800

    test: 💍 create tests for CRUD

---
+++ tests/database.py
@@ -0,0 +1,42 @@
+from fastapi.testclient import TestClient
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from app.main import app
+from app.config import settings
+from app.database import get_db, Base
+
+# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:1234@localhost:5432/fastapi_test'
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+TestingSessionLocal = sessionmaker(
+    autocommit=False, autoflush=False, bind=engine)
+
+
+@pytest.fixture(scope="module")
+def session():
+    print("my session fixture ran")
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@pytest.fixture()
+def client(session):
+    # 依赖关系
+    def override_get_db():
+        try:
+            yield session
+        finally:
+            session.close()
+    app.dependency_overrides[get_db] = override_get_db
+    # 测试前 将执行以上代码
+    yield TestClient(app)
+    # 测试后 将执行一下代码
